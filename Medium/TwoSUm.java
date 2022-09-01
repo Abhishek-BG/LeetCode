95597203df6e313c87f981fd864d1ff06554ad8d
codegeeksofficial <abhigowda0190@gmail.com>
2022-09-01 23:57:07 +0530

    twosum

---
+++ Medium/TwoSUm.java
@@ -0,0 +1,34 @@
+
+/*
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+ */
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { i, map.get(target - nums[i]) };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
--- Medium/main.py
+++
@@ -1,5 +0,0 @@
-#store 5 list values and find the second largest element in the list 
-values = list(map(int,input().split(' ')))
-if 
-elif
-else
